5dbb1ca2b494da784ddd36635b0502ae216a0504
swarajkondhalkar <swarajkondhalkar5789@gmail.com>
2021-04-19 15:52:28 +0530

    added smokeImage

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var gameState = 'onSling'
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,12 +69,16 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gameState!=="launched"){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+    
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched"
 }
 
 function keyPressed(){
